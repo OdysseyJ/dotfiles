d03e2ab031e509c0b95356a34a62842632948fc3
seongwoon.jeong <seongwoon.jeong@tridge.com>
2021-11-10 10:08:18 +0900

    version 1

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
 brew install zsh-syntax-highlighting
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # vim
 brew install neovim
